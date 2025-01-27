8f91934af1b009c0a77551a62dcb931334047c1e
Mark Dunbar <57494763+MarkJamesDunbar@users.noreply.github.com>
2025-01-27 16:10:09 +0000

    uploaded model development

---
+++ model_development/train_model.py
@@ -0,0 +1,215 @@
+import torch
+import torchvision
+import torchviz
+import torchsummary
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import matplotlib.pyplot as plt
+
+import model_utils as mu
+import model_architecture as ma
+
+# Utilise any available GPU resources
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("Current device type: ", device)
+
+#################################################################################################
+# Config Variables
+#################################################################################################
+
+# Filepath Config
+data_folder="../data/"
+artefacts_folder="../model_evaluation/"
+train_file="mnist_train.csv"
+test_file="mnist_test.csv"
+
+# Training Config
+split_size = 0.2
+num_classes = 10
+class_names = {0:"Zero", 1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 6:"Six", 7:"Seven", 8:"Eight", 9:"Nine"}
+img_size = 28
+
+# Model Config
+init_lr = 0.001
+batch_size = 100
+epochs = 1
+
+
+#################################################################################################
+# Data Load & Train Test Split & Prep
+#################################################################################################
+
+train_set   = mu.load_dataset(train_file, data_folder)
+test_set = mu.load_dataset(test_file, data_folder)
+
+# Split the data into train/test splits - split_size defined in config
+train_images, test_images, train_labels, test_labels = train_test_split(train_set.iloc[:, 1:], 
+                                                                     train_set.iloc[:, 0], 
+                                                                     test_size=split_size)
+
+# Reset the index values
+train_images.reset_index(drop=True, inplace=True)
+test_images.reset_index(drop=True, inplace=True)
+train_labels.reset_index(drop=True, inplace=True)
+test_labels.reset_index(drop=True, inplace=True)
+
+train_trans = torchvision.transforms.Compose(([
+    torchvision.transforms.ToPILImage(),
+    torchvision.transforms.RandomCrop(img_size),
+    torchvision.transforms.RandomRotation(20),
+    torchvision.transforms.ColorJitter(brightness=0.2, contrast=0.2),
+    torchvision.transforms.RandomAffine(degrees=5, translate=(0.1, 0.1)),
+    torchvision.transforms.ToTensor(),
+]))
+
+test_trans = torchvision.transforms.Compose(([
+    torchvision.transforms.ToPILImage(),
+    torchvision.transforms.ToTensor(),
+]))
+
+# Perform our transforms and augmentations
+train_set = mu.MNISTDataSet(train_images, img_size, train_labels, train_trans)
+test_set = mu.MNISTDataSet(test_images, img_size, test_labels, test_trans)
+
+# Create dataset dataloaders
+training_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+test_dataloader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False)
+
+
+#################################################################################################
+# Neural Net Definition
+#################################################################################################
+
+net = ma.Network().to(device)
+
+#################################################################################################
+# Neural Net Architecture
+#################################################################################################
+
+torchsummary.summary(net, input_size=(1, 28, 28))
+
+# Eval mode for visualising
+net.eval()
+
+# Dummy input tensor (e.g., 1 grayscale image of size 28x28)
+input_tensor = torch.randn(1, 1, 28, 28)
+
+output = net(input_tensor)
+graph = torchviz.make_dot(output, params=dict(net.named_parameters()))
+graph.render("../model_evaluation/neural_network_graph", format="pdf")
+
+# Revert to training mode
+net.train()
+
+#################################################################################################
+# Optimizer & Scheduler
+#################################################################################################
+
+optimizer = torch.optim.Adam(net.parameters(), lr=init_lr)
+# torch.optimizer = torch.optim.Adagrad(net.parameters(), lr=init_lr)
+# torch.optimizer = torch.optim.SGD(net.parameters(), lr=init_lr, nesterov=True, momentum=0.9)
+
+scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+# scheduler = torch.torch.optim.lr_scheduler.ReduceLROnPlateau(torch.optimizer, mode='min')
+
+#################################################################################################
+# Training
+#################################################################################################
+
+lrs = []
+train_loss = []
+train_acc = []
+test_loss = []
+test_acc = []
+
+# Run our epoch cycles
+for epoch in range(epochs):
+    # Print epoch cycle
+    print(f"Epoch Cycle: {epoch+1}")
+    
+    # Train the model, and append the current learning rate
+    lr, trl, tra = mu.train_model(net, device, optimizer, scheduler, training_dataloader, batch_size)
+    
+    lrs.append(lr)
+    train_loss.append(trl/batch_size)
+    train_acc.append(tra)
+    
+    # Evaluate the model, return the validation loss and validation accuracy
+    tel, tea = mu.eval_model(net, device, test_dataloader, len(test_images), batch_size)
+    
+    test_loss.append(tel/batch_size)
+    test_acc.append(tea)
+
+
+#################################################################################################
+# Evaluation
+#################################################################################################
+
+# Create empty tensor for predictions
+predictions = torch.LongTensor().to(device)
+
+# Use trained model to generate predictions
+for images, _ in test_dataloader:
+    preds = net(images.to(device))
+    predictions = torch.cat((predictions, preds.argmax(dim=1)), dim=0)
+
+# Compute metrics
+accuracy = accuracy_score(test_labels, predictions)
+precision = precision_score(test_labels, predictions, average='weighted')
+recall = recall_score(test_labels, predictions, average='weighted')
+f1 = f1_score(test_labels, predictions, average='weighted')
+
+## Write metrics to file
+with open(artefacts_folder + "metrics.txt", "w") as outfile:
+    outfile.write(f"\nAccuracy: {round(accuracy, 4)}\nPrecision: {round(precision, 4)}\nRecall: {round(recall, 4)}\nF1 Score: {round(f1, 4)}")
+
+# Save confusion matrix plot
+mu.confusion_matrix(num_classes, test_labels, predictions, save_path=artefacts_folder+"confusion_matrix.png")
+
+
+epoch_num = list(range(1, epochs+1))
+
+plt.figure(figsize=(12,7))
+plt.title("Neural Network Learning Rate Curve")
+plt.xlabel("Epoch")
+plt.ylabel("Learning Rate Value")
+plt.yscale("log")
+plt.grid()
+plt.plot(epoch_num, lrs, 'g-')
+plt.xticks(epoch_num)
+plt.savefig(artefacts_folder+"nn_lr_curve.png")
+
+
+nl = [x / 1000 for x in train_loss]
+
+plt.figure(figsize=(12,7))
+plt.title("Neural Network Loss Curves")
+plt.xlabel("Epoch")
+plt.ylabel("Loss Value")
+plt.grid()
+plt.plot(epoch_num, nl, 'b-', label='training loss')
+plt.plot(epoch_num, test_loss, 'r-', label='validation loss')
+plt.legend(loc="upper right")
+plt.xticks(epoch_num)
+plt.savefig(artefacts_folder+"nn_loss_curve.png")
+
+
+plt.figure(figsize=(12,7))
+plt.title("Neural Network Accuracy Curves")
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy (%)")
+plt.ylim(97,100)
+plt.grid()
+plt.plot(epoch_num, train_acc, 'b-', label='training accuracy')
+plt.plot(epoch_num, test_acc, 'r-', label='validation accuracy')
+plt.legend(loc="lower right")
+plt.xticks(epoch_num)
+plt.savefig(artefacts_folder+"nn_acc_curve.png")
+
+#################################################################################################
+# Export Model
+#################################################################################################
+
+torch.save(net, "../app/model/model.pth")
+
